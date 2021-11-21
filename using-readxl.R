a4ec49c435ef7bfc82b852666a8d34b82fb00572
Rod Morgado <rodrigo@zooft.company>
2021-11-20 19:35:45 -0800

    Using lapply to load multiple sheets from an excel file

---
+++ using-readxl.R
@@ -0,0 +1,21 @@
+# The readxl package is already loaded
+
+# Read the sheets, one by one
+pop_1 <- read_excel("urbanpop.xlsx", sheet = 1)
+pop_2 <- read_excel("urbanpop.xlsx", sheet = 2)
+pop_3 <- read_excel("urbanpop.xlsx", sheet = 3)
+
+
+# Put pop_1, pop_2 and pop_3 in a list: pop_list
+pop_list <- list(pop_1, pop_2, pop_3)
+
+# Display the structure of pop_list
+str(pop_list)
+
+# The readxl package is already loaded
+
+# Read all Excel sheets with lapply(): pop_list
+pop_list <- lapply(excel_sheets("urbanpop.xlsx"), read_excel, path = "urbanpop.xlsx")
+
+# Display the structure of pop_list
+str(pop_list)
