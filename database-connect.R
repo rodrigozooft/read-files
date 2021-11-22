675b06a73f1107a31bfdd4cea6dc14bf505f68a8
Rod Morgado <rodrigo@zooft.company>
2021-11-21 18:29:05 -0800

    creating a connection between R and the database

---
+++ database-connect.R
@@ -0,0 +1,12 @@
+# Connecting R with a database using DBI driver
+
+# Load the DBI package
+library(DBI)
+
+# Edit dbConnect() call
+con <- dbConnect(RMySQL::MySQL(), 
+                 dbname = "tweater", 
+                 host = "courses.csrrinzqubik.us-east-1.rds.amazonaws.com", 
+                 port = 3306,
+                 user = "student",
+                 password = "datacamp")
