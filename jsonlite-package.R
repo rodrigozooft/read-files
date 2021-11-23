5e48be719692eec6204c1165c922fd0d75c3a708
Rod Morgado <rodrigo@zooft.company>
2021-11-22 20:41:39 -0800

    using fromJSON to parse json file and save it as a list

--- jsonlite-package.R
+++ jsonlite-package.R
@@ -22,3 +22,21 @@ quandl_data <- fromJSON(quandl_url)
 
 # Print structure of quandl_data
 str(quandl_data)
+
+# The package jsonlite is already loaded
+
+# Definition of the URLs
+url_sw4 <- "http://www.omdbapi.com/?apikey=72bc447a&i=tt0076759&r=json"
+url_sw3 <- "http://www.omdbapi.com/?apikey=72bc447a&i=tt0121766&r=json"
+
+# Import two URLs with fromJSON(): sw4 and sw3
+sw4 <- fromJSON(url_sw4)
+sw3 <- fromJSON(url_sw3)
+
+# Print out the Title element of both lists
+sw4$Title
+sw3$Title
+
+
+# Is the release year of sw4 later than sw3?
+sw4$Year > sw3$Year
