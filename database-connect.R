e49fed8841fe26a132aab152038512f359ee77a2
Rod Morgado <rodrigo@zooft.company>
2021-11-22 19:01:52 -0800

    using dbSendQuery and dbFetch to perform a query to the database

--- database-connect.R
+++ database-connect.R
@@ -90,3 +90,23 @@ short <- dbGetQuery(con, "SELECT id, name FROM users WHERE CHAR_LENGTH(name) < 5
 
 # Print short
 short
+
+# Connect to the database
+library(DBI)
+con <- dbConnect(RMySQL::MySQL(),
+                 dbname = "tweater",
+                 host = "courses.csrrinzqubik.us-east-1.rds.amazonaws.com",
+                 port = 3306,
+                 user = "student",
+                 password = "datacamp")
+
+# Send query to the database
+res <- dbSendQuery(con, "SELECT * FROM comments WHERE user_id > 4")
+
+# Use dbFetch() twice
+dbFetch(res, n = 2)
+dbFetch(res)
+
+
+# Clear res
+dbClearResult(res)
