8537e68a3c44a61fb98a8dff58a61cc7c54293a9
Rod Morgado <rodrigo@zooft.company>
2021-11-22 19:17:41 -0800

    using two different packages to import data from a http request

--- http-requests-with-r.R
+++ http-requests-with-r.R
@@ -15,3 +15,19 @@ potatoes <- read_tsv(url_delim)
 # Print pools and potatoes
 pools
 potatoes
+
+# https URL to the swimming_pools csv file.
+url_csv <- "https://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/swimming_pools.csv"
+
+# Import the file using read.csv(): pools1
+pools1 <- read.csv(url_csv)
+
+# Load the readr package
+library(readr)
+
+# Import the file using read_csv(): pools2
+pools2 <- read_csv(url_csv)
+
+# Print the structure of pools1 and pools2
+str(pools1)
+str(pools2)
