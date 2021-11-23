774d5a246c9b18c54db37cd3843c4a39002dffca
Rod Morgado <rodrigo@zooft.company>
2021-11-22 16:03:43 -0800

    adding a connection to the db in mysql

--- database-connect.R
+++ database-connect.R
@@ -29,4 +29,19 @@ table_names <- dbListTables(con)
 tables <- lapply(table_names, dbReadTable, conn = con)
 
 # Print out tables
-tables                
+tables
+
+# Connect to the database
+library(DBI)
+con <- dbConnect(RMySQL::MySQL(),
+                 dbname = "tweater",
+                 host = "courses.csrrinzqubik.us-east-1.rds.amazonaws.com",
+                 port = 3306,
+                 user = "student",
+                 password = "datacamp")
+
+# Import tweat_id column of comments where user_id is 1: elisabeth
+elisabeth <- dbGetQuery(con, "SELECT tweat_id FROM comments WHERE user_id = 1")
+
+# Print elisabeth
+elisabeth
