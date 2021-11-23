03bb1220a56f55f4c66556bffc22305ad331ef75
Rod Morgado <rodrigo@zooft.company>
2021-11-22 20:38:15 -0800

    using jsonlite to get financial data from the web

--- jsonlite-package.R
+++ jsonlite-package.R
@@ -11,3 +11,14 @@ wine <- fromJSON(wine_json)
 
 # Print structure of wine
 str(wine)
+
+# jsonlite is preloaded
+
+# Definition of quandl_url
+quandl_url <- "https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?auth_token=i83asDsiWUUyfoypkgMz"
+
+# Import Quandl data: quandl_data
+quandl_data <- fromJSON(quandl_url)
+
+# Print structure of quandl_data
+str(quandl_data)
